c6e852eb78c89712c68d196782fdf8cc91266243
Rober Martín H <rmh.dev@gmail.com>
2012-04-05 13:04:08 +0200

    refactoring script

--- scrapping/buscadorbautizos.py
+++ scrapping/buscadorbautizos.py
@@ -6,30 +6,45 @@ from bs4 import BeautifulSoup
 import re
 import codecs
 
-uri = 'http://dos.cgssl.net/Zumarraga/ArchivoHistorico/BuscadorBautizos.aspx'
+def get_uri():
+    return 'http://httpbin.org/post'
+    #return 'http://dos.cgssl.net/Zumarraga/ArchivoHistorico/BuscadorBautizos.aspx'
 
-parameters_aspx = {
-    '__EVENTTARGET'     : 'ctl00$ContentPlaceHolder1$gvResultados',
-    '__EVENTARGUMENT'   : 'Page$1',
-    '__LASTFOCUS'       : '',
-    '__VIEWSTATE'       : '',
-    '__EVENTVALIDATION' : ''
-}
+def get_default_parameters(): 
+    return {
+        '__EVENTTARGET'     : 'ctl00$ContentPlaceHolder1$gvResultados',
+        '__EVENTARGUMENT'   : 'Page$1',
+        '__LASTFOCUS'       : '',
+        '__VIEWSTATE'       : '',
+        '__EVENTVALIDATION' : '',
 
-parameters_search = {
-    'ctl00$SeleccionIdioma'                             : 'es-ES',
-    'ctl00$ContentPlaceHolder1$txtNombre'               : '',
-    'ctl00$ContentPlaceHolder1$txtApellido1'            : '',
-    'ctl00$ContentPlaceHolder1$txtApellido2'            : '',
-    'ctl00$ContentPlaceHolder1$ddlSexo'                 : '-',
-    'ctl00$ContentPlaceHolder1$txtToponimo'             : '',
-    'ctl00$ContentPlaceHolder1$txtPoblacionPariente'    : '',
-    'ctl00$ContentPlaceHolder1$txtBusquedaLibre'        : '',
-    'ctl00$ContentPlaceHolder1$txtFechaIni'             : '',
-    'ctl00$ContentPlaceHolder1$txtFechaFin'             : '',
-    'ctl00$ContentPlaceHolder1$cmdBuscar'               : 'Buscar',
-}
+        'ctl00$SeleccionIdioma'                             : 'es-ES',
+        'ctl00$ContentPlaceHolder1$txtNombre'               : '',
+        'ctl00$ContentPlaceHolder1$txtApellido1'            : '',
+        'ctl00$ContentPlaceHolder1$txtApellido2'            : '',
+        'ctl00$ContentPlaceHolder1$ddlSexo'                 : '-',
+        'ctl00$ContentPlaceHolder1$txtToponimo'             : '',
+        'ctl00$ContentPlaceHolder1$txtPoblacionPariente'    : '',
+        'ctl00$ContentPlaceHolder1$txtBusquedaLibre'        : '',
+        'ctl00$ContentPlaceHolder1$txtFechaIni'             : '',
+        'ctl00$ContentPlaceHolder1$txtFechaFin'             : '',
+        'ctl00$ContentPlaceHolder1$cmdBuscar'               : 'Buscar'
+    }
 
+def get_http_headers():
+   return {
+        'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; es-ES)'
+    }
+
+def prepare_parameters():
+    f = open('test.html', 'r')
+    html = f.read()
+    f.close()
+    soup = BeautifulSoup(html)
+    parameters = get_default_parameters()
+    parameters['__VIEWSTATE']          = soup.find('input', id='__VIEWSTATE').get('value')
+    parameters['__EVENTVALIDATION']    = soup.find('input', id='__EVENTVALIDATION').get('value') 
+    return parameters
 
 def get_data_from_html(html):
     soup = BeautifulSoup(html)
@@ -53,27 +68,18 @@ def get_data_from_cols(cols):
     birthday = cols[3].string.strip()
     return '"%s";"%s";"%s";"%s";"%s"' % (id, name, surname1, surname2, birthday)
 
-if __name__ == "__main__":
-    f = open('test.html', 'r')
-    html = f.read()
+def parse_and_save_to_file(html, file_name): 
+    f = codecs.open( file_name, 'w', 'utf-8' )
+    f.write("\n".join(get_data_from_html(html)))
     f.close()
 
-    soup = BeautifulSoup(html)
-    parameters_aspx['__VIEWSTATE']          = soup.find('input', id='__VIEWSTATE').get('value')
-    parameters_aspx['__EVENTVALIDATION']    = soup.find('input', id='__EVENTVALIDATION').get('value')
-    parameters = dict( parameters_aspx.items() + parameters_search.items() )
-
-    headers = {
-        'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; es-ES)'
-    }
-    req = requests.post(uri, data=parameters, headers=headers)
+if __name__ == "__main__":
+    req = requests.post(get_uri(), data=prepare_parameters(), headers=get_http_headers())
     if req.status_code != 200:
         print "error!"
         exit()
-
     html = req.text
-    #print html
+    print html
+    exit()
+    parse_and_save_to_file(html, 'result.txt')
 
-    f = codecs.open( 'result.txt', 'w', 'utf-8' )
-    f.write("\n".join(get_data_from_html(html)))
-    f.close()
